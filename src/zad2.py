b061f40362d582f9e8f243f935fbc8bccbc46654
Sienkowski <p.sienkowski@onet.pl>
2020-11-10 22:05:38 +0100

    zad2 doctest

--- src/zad2.py
+++ src/zad2.py
@@ -22,12 +22,62 @@ class Checker:
         False
         >>> c.ValidPassword("haslomalarskie128e")
         False
+        >>> c.ValidPassword("haslo")
+        False
+        >>> c.ValidPassword("ARBITER")
+        False
+        >>> c.ValidPassword(23)
+        Traceback (most recent call last):
+          File "C:\Python\lib\doctest.py", line 1337, in __run
+            compileflags, 1), test.globs)
+          File "<doctest __main__.Checker.ValidPassword[11]>", line 1, in <module>
+            c.ValidPassword(23)
+          File "C:/Users/Sieniu/studia/testowanie_automatyczne/laboratorium-6-Sienkowski99/src/zad2.py", line 33, in ValidPassword
+            raise ValueError('Your password must be a string')
+        ValueError: Your password must be a string
+        >>> c.ValidPassword(None)
+        Traceback (most recent call last):
+          File "C:\Python\lib\doctest.py", line 1337, in __run
+            compileflags, 1), test.globs)
+          File "<doctest __main__.Checker.ValidPassword[11]>", line 1, in <module>
+            c.ValidPassword(23)
+          File "C:/Users/Sieniu/studia/testowanie_automatyczne/laboratorium-6-Sienkowski99/src/zad2.py", line 33, in ValidPassword
+            raise ValueError('Your password must be a string')
+        ValueError: Your password must be a string
+        >>> c.ValidPassword(False)
+        Traceback (most recent call last):
+          File "C:\Python\lib\doctest.py", line 1337, in __run
+            compileflags, 1), test.globs)
+          File "<doctest __main__.Checker.ValidPassword[11]>", line 1, in <module>
+            c.ValidPassword(23)
+          File "C:/Users/Sieniu/studia/testowanie_automatyczne/laboratorium-6-Sienkowski99/src/zad2.py", line 33, in ValidPassword
+            raise ValueError('Your password must be a string')
+        ValueError: Your password must be a string
         """
+        specialCharacters = "!@#$%^&*()_+-="
+        numbers = "0123456789"
+        alphabet = "aąbcćdeęfghijklłmnńoópqrsśtuvwxyzźż"
         if not isinstance(given_string, str):
             raise ValueError('Your password must be a string')
         if len(given_string) < 8:
-            raise ValueError('Your password must be at least 8 characters long')
-        return
+            return False
+        requirements = 3
+        for char in specialCharacters:
+            if char in given_string:
+                requirements -= 1
+                break
+        for char in numbers:
+            if char in given_string:
+                requirements -= 1
+                break
+        for char in alphabet.upper():
+            if char in given_string:
+                requirements -= 1
+                break
+        if requirements == 0:
+            return True
+        else:
+            return False
 
 
 if __name__ == "__main__":
