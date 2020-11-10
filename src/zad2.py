0cd37367169772d374570f9221a484e2adbfc462
Sienkowski <p.sienkowski@onet.pl>
2020-11-10 17:49:19 +0100

    zad2 changed code structure, added few tests

--- src/zad2.py
+++ src/zad2.py
@@ -1,15 +1,33 @@
 #Działa na Pythonie 3.8.X
-class Password:
+class Checker:
     def ValidPassword(self, given_string):
         """
-        Return how many nucleotides are different on the same indexes.
-        >>>
+        Checks if given_string (password) has meets the requirements.
+        >>> c = Checker()
+        >>> c.ValidPassword("halibutZZiemniakamimmmmmmPycha!!!111oneone")
+        True
+        >>> c.ValidPassword("HumanistaBeletrysta2137()")
+        True
+        >>> c.ValidPassword("HassaN0)")
+        True
+        >>> c.ValidPassword("HASLo!23")
+        True
+        >>> c.ValidPassword("Haslo123@")
+        True
+        >>> c.ValidPassword("haaaaaaaaaaaaaaaa)")
+        False
+        >>> c.ValidPassword("!!!!!!!!!!!!!!!!")
+        False
+        >>> c.ValidPassword("237942789374892738")
+        False
+        >>> c.ValidPassword("haslomalarskie128e")
+        False
         """
         if not isinstance(given_string, str):
             raise ValueError('Your password must be a string')
         if len(given_string) < 8:
             raise ValueError('Your password must be at least 8 characters long')
-        return 
+        return
 
 
 if __name__ == "__main__":
